c0b19a0fec14625db3c2c0c71eb8a91dba9a9c40
Glen Martin <glen18martin@gmail.com>
2017-01-13 22:05:08 +0530

    testing needed

--- js/load.js
+++ js/load.js
@@ -3,7 +3,7 @@ let loadState = {
         game.load.image('tree_belly', 'assets/tre.png');
         game.load.image('tree_foot', 'assets/bark.png');
         game.load.image('grass', 'assets/grass.png');
-        game.load.spritesheet('sams', 'assets/sam.png', 64, 64, 36);
+        game.load.spritesheet('sams', 'assets/sam.png', 64, 64, 36); //64,64 old values
         game.time.advancedTiming = true;
 
 
