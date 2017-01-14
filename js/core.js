15cc4b115a11215b8bae7b12337e6980c59b1c40
roydl7 <roydl.net@gmail.com>
2017-01-14 13:14:25 +0530

    mod-1

--- js/core.js
+++ js/core.js
@@ -8,18 +8,14 @@ var feet;
 let playState = {
     create: function create() {
         game.physics.startSystem(Phaser.Physics.ARCADE);
-        
+
+
         game.stage.backgroundColor = GameSettings.backgroundColor;
         game.world.setBounds(-GameSettings.bounds/2, -GameSettings.bounds/2, GameSettings.bounds, GameSettings.bounds);
+
         cursors = game.input.keyboard.createCursorKeys();
-   
+        movementKeys = Movement.getMovementKeys();
         
-        movementKeys = {
-            up: game.input.keyboard.addKey(Phaser.Keyboard.W),
-            down: game.input.keyboard.addKey(Phaser.Keyboard.S),
-            left: game.input.keyboard.addKey(Phaser.Keyboard.A),
-            right: game.input.keyboard.addKey(Phaser.Keyboard.D),
-        };
  
         game.add.tileSprite(-GameSettings.bounds, -GameSettings.bounds, GameSettings.bounds * 2, GameSettings.bounds * 2, 'grass');
          
@@ -31,11 +27,11 @@ let playState = {
         tomatoes = game.add.group();
         tomatoes.enableBody = true;
 
-         sam = game.add.sprite(800, 400, 'sams');
-                sam.enableBody = true;
-                sam.scale.setTo(1.5,1.5);
-                game.physics.arcade.enable(sam);
-                sam.body.setSize(50, 42, 7, 15); //check
+        sam = game.add.sprite(800, 400, 'sams');
+        sam.enableBody = true;
+        sam.scale.setTo(1.5,1.5);
+        game.physics.arcade.enable(sam);
+        sam.body.setSize(50, 42, 7, 15); //check
 
         belly = game.add.group();
 
@@ -76,6 +72,7 @@ let playState = {
                 cursors = game.input.keyboard.createCursorKeys();
                 game.camera.follow(sam);
     },
+
     update:function update() {
         
         game.physics.arcade.collide(sam, feet);
@@ -84,67 +81,19 @@ let playState = {
         game.physics.arcade.collide(sam, tomatoes);
         game.physics.arcade.overlap(sam, tomatoes, function() { alert("spooch"); }, null, this);
         //game.debug.cameraInfo(game.camera, 32, 32);
-        //game.camera.x++;
+
+        
+        
+        Movement.movementHandler(cursors, movementKeys, sam);
         game.debug.text(game.time.fps + "fps, "+ game.time.elapsed + " ms. Min: " + game.time.fpsMin + " Max:" + game.time.fpsMax , 2, 14, "#00ff00");
-        //movementHandler();
-        sam.body.velocity.x = 0;
-                sam.body.velocity.y = 0;
-                
-                    if(cursors.right.isDown){
-                            sam.body.velocity.x = 250;
-                            sam.animations.play('right');
-                    }
-                    else if (cursors.up.isDown)
-                    {
-                        //  Move to the left
-                        sam.body.velocity.y = -250;
-                        sam.animations.play('up');
-                    }
-                    
-                    else if (cursors.down.isDown)
-                    {
-                        //  Move to the left
-                        sam.body.velocity.y = 250;
-                        sam.animations.play('down');
-                        //player.animations.play('left');
-                    }
-                    else if (cursors.left.isDown)
-                    {
-                        //  Move to the left
-                        sam.body.velocity.x = -250;
-                        sam.animations.play('left');
-                        //player.animations.play('left');
-                    }
-                /* 
-    adding provisions for diagonal movements -- simultaneous execution needs to be checked
-                else if(cursors.right.isDown && cursors.up.isDown){
-                            sam.body.velocity.x = 250;
-                            sam.body.velocity.y = -250;
-                            sam.animations.play('right');
-                            sam.animations.play('up');
-                    }*/
-                    else
-                    {
-                        //  Stand still
-                        sam.animations.stop();
-                        sam.frame = 20;
-                    }
         
     }
 };
-function abox() {
-    var graphics = game.add.graphics(100, 100);
-    graphics.beginFill(0xFF0000);
-    graphics.lineStyle(3, 0xffd900, 1);
-    graphics.drawRect(-164, -164, 164, 164);
-    return graphics;
-}
-function movementHandler() {
-    game.camera.y -= cursors.up.isDown || movementKeys.up.isDown ? 4 : 0;
-    game.camera.y += cursors.down.isDown || movementKeys.down.isDown ? 4 : 0;
-    game.camera.x -= cursors.left.isDown || movementKeys.left.isDown ? 4 : 0;
-    game.camera.x += cursors.right.isDown || movementKeys.right.isDown ? 4 : 0;
-}
+
+
+
+
+
 game.state.add('boot', bootState);
 game.state.add('load', loadState);
 game.state.add('play', playState);
