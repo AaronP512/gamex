8984a75bf4c6b4ae017de6a1866dbc2c54e5b784
mr_swagster <acp.aaron@gmail.com>
2017-01-30 15:13:05 +0530

    movement

--- js/movement.js
+++ js/movement.js
@@ -14,29 +14,141 @@ class Movement {
         let keysBeingHeld = false;
 
         sam.body.velocity.x = sam.body.velocity.y = 0;
-    
-        if(cursors.up.isDown || movementKeys.up.isDown) {
+    	// //left
+     //    if(cursors.left.isDown || movementKeys.left.isDown) {
+     //        sam.body.velocity.x = -speed;
+     //        sam.animations.play('left');
+     //        keysBeingHeld = true;
+     //    }
+    	// //up
+     //    if(cursors.up.isDown || movementKeys.up.isDown) {
+     //        sam.body.velocity.y = -speed;
+     //        sam.animations.play('up');
+     //        keysBeingHeld = true;
+     //        console.log('up');
+     //    }
+     //    //up-left
+     //    if((cursors.up.isDown || movementKeys.up.isDown)&&(cursors.left.isDown || movementKeys.left.isDown)) {
+     //        sam.body.velocity.x = -speed;
+     //        sam.body.velocity.y = -speed;
+     //        sam.animations.play('left');
+     //        keysBeingHeld = true;
+     //        console.log('upleft');
+     //    }
+     //    //up-right
+     //    if((cursors.up.isDown || movementKeys.up.isDown)&&(cursors.right.isDown || movementKeys.right.isDown)) {
+     //        console.log(cursors.up.isDown || movementKeys.up.isDown);
+     //        console.log(cursors.right.isDown || movementKeys.right.isDown);
+     //        sam.body.velocity.x = speed;
+     //        sam.body.velocity.y = -speed;
+     //        sam.animations.play('right');
+     //        keysBeingHeld = true;
+     //        console.log('upright');
+     //    }
+     //    //down-left
+     //    if((cursors.down.isDown || movementKeys.down.isDown)&&(cursors.left.isDown || movementKeys.left.isDown)) {
+     //        sam.body.velocity.x = -speed;
+     //        sam.body.velocity.y = speed;
+     //        sam.animations.play('left');
+     //        keysBeingHeld = true;
+     //        console.log('downleft');
+     //    }
+     //    //down-right
+     //    if((cursors.down.isDown || movementKeys.down.isDown)&&(cursors.right.isDown || movementKeys.right.isDown)) {
+            
+     //        sam.body.velocity.x = speed;
+     //        sam.body.velocity.y = speed;
+     //        sam.animations.play('right');
+     //        keysBeingHeld = true;
+     //        console.log('down-right');
+     //    }
+     //    //down
+     //    if(cursors.down.isDown || movementKeys.down.isDown) {
+     //        sam.body.velocity.y = speed;
+     //        sam.animations.play('down');
+     //        keysBeingHeld = true;
+     //        console.log('down');
+     //    }
+        
+     //    //right
+     //    if(cursors.right.isDown || movementKeys.right.isDown) {
+     //        sam.body.velocity.x = speed;
+     //        sam.animations.play('right');
+     //        keysBeingHeld = true;
+     //    }
+     //    else {
+     //        this.lastKeysHeld = true;
+     //    }
+
+     //    if(keysBeingHeld == false) {
+     //        if(this.lastKeysHeld) { //Stop animations only if movementkeys were pressed in  LAST CALL, so avoid stopping fighting anims
+     //            sam.animations.stop();
+     //            sam.frame = 26; 
+     //            this.lastKeysHeld = false;
+     //        }
+            
+     //    } 
+
+ 	if(cursors.up.isDown || movementKeys.up.isDown) {
+        if(cursors.left.isDown || movementKeys.left.isDown){
+        	sam.body.velocity.x = -speed;
+            sam.body.velocity.y = -speed;
+            sam.animations.play('left');
+            keysBeingHeld = true;
+            console.log('upleft');
+        }
+        else if(cursors.right.isDown || movementKeys.right.isDown){
+        	sam.body.velocity.x = speed;
+            sam.body.velocity.y = -speed;
+            sam.animations.play('right');
+            keysBeingHeld = true;
+            console.log('upright');
+        }
+ 		else{
             sam.body.velocity.y = -speed;
             sam.animations.play('up');
             keysBeingHeld = true;
-        }
+    	}
+    }
 
-        if(cursors.down.isDown || movementKeys.down.isDown) {
+	if(cursors.down.isDown || movementKeys.down.isDown) {
+        if(cursors.left.isDown || movementKeys.left.isDown){
+        	sam.body.velocity.x = -speed;
+            sam.body.velocity.y = speed;
+            sam.animations.play('left');
+            keysBeingHeld = true;
+            console.log('upleft');
+        }
+        else if(cursors.right.isDown || movementKeys.right.isDown){
+        	sam.body.velocity.x = speed;
+            sam.body.velocity.y = speed;
+            sam.animations.play('right');
+            keysBeingHeld = true;
+            console.log('upright');
+        }
+ 		else{
             sam.body.velocity.y = speed;
             sam.animations.play('down');
             keysBeingHeld = true;
-        }
+    	}
+    }
 
-        if(cursors.left.isDown || movementKeys.left.isDown) {
+
+    else if(cursors.left.isDown || movementKeys.left.isDown) {
             sam.body.velocity.x = -speed;
             sam.animations.play('left');
             keysBeingHeld = true;
         }
 
-        if(cursors.right.isDown || movementKeys.right.isDown) {
-            sam.body.velocity.x = speed;
-            sam.animations.play('right');
-            keysBeingHeld = true;
+        
+    //right
+    else if(cursors.right.isDown || movementKeys.right.isDown) {
+        sam.body.velocity.x = speed;
+        sam.animations.play('right');
+        keysBeingHeld = true;
+    }
+    else {
+            this.lastKeysHeld = true;
         }
 
         if(keysBeingHeld == false) {
@@ -46,10 +158,11 @@ class Movement {
                 this.lastKeysHeld = false;
             }
             
-        } else {
-            this.lastKeysHeld = true;
-        }
+        } 
 
+
+
+        
         //console.log(sam.animations.currentAnim.name);
         if(typeof socket !== 'undefined' && keysBeingHeld) { //' && socket
 
